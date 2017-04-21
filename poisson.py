b8af2a040338432095c5151bb5fc7c8bbbe57f1b
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-04-21 15:39:38 +0100

    Add hybrid mixed poisson solve

--- poisson.py
+++ poisson.py
@@ -37,3 +37,61 @@ def run_primal_poisson(r, d, quads=False):
     solve(a == L, uh, bcs=bcs, solver_parameters=params)
 
     return uh
+
+
+def run_hybrid_poisson(r, d, quads=False):
+    """Runs a 3D solver for the mixed Poisson equation. This
+    solves the Poisson equation using a hybrided mixed method
+    solver.
+
+    :arg r: An ``int`` for computing the mesh resolution.
+    :arg d: An ``int`` denoting the degree of approximation.
+    :arg quads: A ``bool`` specifying whether to use a quad mesh.
+
+    Returns: The scalar solution.
+    """
+
+    base = UnitSquareMesh(2 ** r, 2 ** r, quadrilateral=quads)
+    layers = 2 ** r
+    mesh = ExtrudedMesh(base, layers, layer_height=1.0 / layers)
+    n = FacetNormal(mesh)
+
+    if quads:
+        RT = FiniteElement("RTCF", quadrilateral, d)
+        DG_v = FiniteElement("DG", interval, d - 1)
+        DG_h = FiniteElement("DQ", quadrilateral, d - 1)
+        CG = FiniteElement("CG", interval, d)
+        U = FunctionSpace(mesh, "DQ", d - 1)
+
+    else:
+        RT = FiniteElement("RT", triangle, d)
+        DG_v = FiniteElement("DG", interval, d - 1)
+        DG_h = FiniteElement("DG", triangle, d - 1)
+        CG = FiniteElement("CG", interval, d)
+        U = FunctionSpace(mesh, "DG", d - 1)
+
+    HDiv_ele = EnrichedElement(HDiv(TensorProductElement(RT, DG_v)),
+                               HDiv(TensorProductElement(DG_h, CG)))
+    V = FunctionSpace(mesh, HDiv_ele)
+    W = V * U
+
+    sigma, u = TrialFunctions(W)
+    tau, v = TestFunctions(W)
+    a = (dot(sigma, tau) - div(tau) * u + div(sigma) * v) * dx
+
+    f = Function(U)
+    f.assign(0.0)
+    L = -42.0 * dot(tau, n) * ds_t
+
+    wh = Function(W)
+    params = {"mat_type": "matfree",
+              "pc_type": "python",
+              "pc_python_type": "firedrake.HybridizationPC",
+              "hybridization_pc_type": "hypre",
+              "hybridization_pc_hypre_type": "boomeramg",
+              "hybridization_ksp_rtol": 1e-8,
+              "hybridization_projector_tolerance": 1e-14}
+    solve(a == L, wh, solver_parameters=params)
+    sigmah, uh = wh.split()
+
+    return sigmah, uh
--- run.py
+++ run.py
@@ -3,25 +3,26 @@ from __future__ import absolute_import, print_function, division
 from firedrake import *
 
 from helmholtz import run_helmholtz
-from poisson import run_primal_poisson
+from poisson import run_primal_poisson, run_hybrid_poisson
 
 
 for flag in [False, True]:
-    vel, pr = run_helmholtz(3, 1, quads=flag)
-
-    if flag:
-        name = "2D-helmholtz-quad"
-    else:
-        name = "2D-helmholtz"
-
-    File(name + ".pvd").write(vel, pr)
-
-for flag in [False, True]:
-    u = run_primal_poisson(3, 1, quads=flag)
+    u = run_primal_poisson(4, 1, quads=flag)
+    sigmah, uh = run_hybrid_poisson(4, 1, quads=flag)
+    CG_element = u.function_space().ufl_element()
+    mesh = uh.function_space().mesh()
+    uh = interpolate(uh, FunctionSpace(mesh, CG_element))
+    vel, pr = run_helmholtz(4, 1, quads=flag)
 
     if flag:
         name = "3D-primalpoisson-quad"
+        name2 = "3D-hybridpoisson-quad"
+        name3 = "2D-helmholtz-quad"
     else:
         name = "3D-primalpoisson"
+        name2 = "3D-hybridpoisson"
+        name3 = "2D-helmholtz"
 
     File(name + ".pvd").write(u)
+    File(name2 + ".pvd").write(sigmah, uh)
+    File(name3 + ".pvd").write(vel, pr)
