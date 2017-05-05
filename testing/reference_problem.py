b1bc1e0230983fa8b3aed68c4c8508e96228b0c1
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-05 15:40:07 +0100

    Generate comparisons between hybrid and non-hybrid solves

--- testing/reference_problem.py
+++ testing/reference_problem.py
@@ -4,9 +4,9 @@ from firedrake import *
 
 qflag = False
 degree = 1
-hybrid = True
+res = 2
 
-mesh = UnitSquareMesh(2, 2, quadrilateral=qflag)
+mesh = UnitSquareMesh(res, res, quadrilateral=qflag)
 n = FacetNormal(mesh)
 
 if qflag:
@@ -34,17 +34,29 @@ L = -f*q*dx + 42*dot(v, n)*ds(4)
 
 bcs = [DirichletBC(W.sub(0), Expression(("0", "0")), (1, 2))]
 
-if hybrid:
-    params = {"ksp_type": "preonly",
-              "mat_type": "matfree",
-              "pc_type": "python",
-              "pc_python_type": "firedrake.HybridizationPC",
-              "hybridization_ksp_type": "preonly",
-              "hybridization_pc_type": "lu",
-              "hybridization_projector_tolerance": 1e-10}
-else:
-    params = {"ksp_type": "gmres",
-              "ksp_rtol": 1e-10}
+usol = []
+psol = []
+for hybrid in [False, True]:
+    if hybrid:
+        params = {"ksp_type": "preonly",
+                  "mat_type": "matfree",
+                  "pc_type": "python",
+                  "pc_python_type": "firedrake.HybridizationPC",
+                  "hybridization_ksp_type": "preonly",
+                  "hybridization_pc_type": "lu",
+                  "hybridization_projector_tolerance": 1e-10}
+        suffix = "-hybrid"
+    else:
+        params = {"ksp_type": "gmres",
+                  "ksp_rtol": 1e-10}
+        suffix = ""
 
-w = Function(W)
-solve(a == L, w, bcs=bcs, solver_parameters=params)
+    w = Function(W)
+    solve(a == L, w, bcs=bcs, solver_parameters=params)
+    udat, pdat = w.split()
+    uh = Function(V, name="velocity"+suffix).assign(udat)
+    ph = Function(U, name="pressure"+suffix).assign(pdat)
+    usol.append(uh)
+    psol.append(ph)
+
+File("natural_bc_test.pvd").write(usol[0], usol[1], psol[0], psol[1])
