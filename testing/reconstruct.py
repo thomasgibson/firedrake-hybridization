effb5d2fda33240c0389fb98fc15a6a6c10c3474
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-05 15:55:26 +0100

    Clean up reconstruct

--- testing/reconstruct.py
+++ testing/reconstruct.py
@@ -15,38 +15,11 @@ fct = as_vector([x[0] ** 2, x[1] ** 2])
 u0 = Function(U).project(fct)
 u = Function(Ud).project(fct)
 
-# Tr = FunctionSpace(mesh, "HDiv Trace", 0)
-
-# gammar = TestFunction(Tr)
-# lambdar = TrialFunction(Tr)
-
-# n = FacetNormal(mesh)
-
-# a_dS = lambdar('+')*gammar('+')*dS + lambdar*gammar*ds
-# l_dS = gammar('+')*0.5*jump(dot(u, n))*dS + gammar*0.5*dot(u, n)*ds
-# f = Function(Tr)
-# # A = assemble(a_dS).M.values
-# # b = assemble(l_dS).dat.data
-
-# solve(a_dS == l_dS, f)
-
-# ubar = TrialFunction(U)
-
-# a = gammar('+')*dot(ubar, n)('+')*dS + gammar*dot(ubar, n)*ds
-# l = gammar('+')*f('+')*dS + gammar*f*ds
-
-# a_global = gammar*jump(ubar, n=n)*dS + gammar*dot(ubar, n)*ds
-
-# M = Tensor(a)
-# q = Tensor(l)
-# x = assemble(M.inv*q)
-
 ans = Function(U)
-# solve(a == l, ans)
 
 count_code = """
 for (int i; i<count.dofs; ++i) {
-    count[i][0] += 1;
+    count[i][0] += 1.0;
 }
 """
 
