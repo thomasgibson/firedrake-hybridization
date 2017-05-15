ed7ff1ba2f1c516c7041d7ca2c6b47eeba631568
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-15 05:54:40 +0100

    Modify schur-nullspace test

--- testing/schur_nullspace.py
+++ testing/schur_nullspace.py
@@ -1,13 +1,15 @@
+from __future__ import absolute_import, print_function, division
+
 from firedrake import *
 from firedrake.slate.preconditioners import create_schur_nullspace
 import numpy as np
 
-mesh = UnitCubedSphereMesh(2)
+mesh = UnitIcosahedralSphereMesh(2)
 mesh.init_cell_orientations(SpatialCoordinate(mesh))
 
 n = FacetNormal(mesh)
 
-V = FunctionSpace(mesh, "RTCF", 1)
+V = FunctionSpace(mesh, "RT", 1)
 Q = FunctionSpace(mesh, "DG", 0)
 
 W = V*Q
@@ -29,29 +31,37 @@ gamma = TestFunction(Vt)
 sigma, _ = TrialFunctions(W_d)
 
 K = Tensor(gamma('+') * dot(sigma, n) * dS)
+k = assemble(K*K.T)
+print(k.M.values)
 
 A = assemble(a, mat_type="aij")
 nullspace = MixedVectorSpaceBasis(W, [W[0], VectorSpaceBasis(constant=True)])
 nullspace._build_monolithic_basis()
 A.petscmat.setNullSpace(nullspace._nullspace)
 
-Snullsp = create_schur_nullspace(A.petscmat, K * atilde.inv,
+Snullsp = create_schur_nullspace(A.petscmat, -(K * K.T).inv * K * atilde,
                                  W, W_d, Vt,
                                  COMM_WORLD)
 
 v = Snullsp.getVecs()[0]
-print "Computed nullspace of S (min, max, norm)", v.array_r.min(), v.array_r.max(), v.norm()
+print(
+    "Computed nullspace of S (min, max, norm)", v.array_r.min(), v.array_r.max(), v.norm()
+)
 
 S = K * atilde.inv * K.T
 
 u, s, v = np.linalg.svd(assemble(S, mat_type="aij").M.values)
 
 singular_vector = v[-1]
-print "Actual nullspace of S (min, max, norm)", singular_vector.min(), singular_vector.max(), np.linalg.norm(singular_vector)
+print(
+    "Actual nullspace of S (min, max, norm)", singular_vector.min(), singular_vector.max(), np.linalg.norm(singular_vector)
+)
 
 u, s, v = np.linalg.svd(A.M.handle[:, :])
 
 offset = V.dof_dset.size
 
 singular_vector = v[-1][offset:]
-print "Nullspace of original operator (min, max, norm)", singular_vector.min(), singular_vector.max(), np.linalg.norm(singular_vector)
+print(
+    "Nullspace of original operator (min, max, norm)", singular_vector.min(), singular_vector.max(), np.linalg.norm(singular_vector)
+)
