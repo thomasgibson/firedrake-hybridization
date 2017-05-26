1f7f058d97c6599401558a280d85affed7fb5394
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-26 12:24:03 +0100

    Update projection experiment for HDiv functions

--- testing/hdiv_proj.py
+++ testing/hdiv_proj.py
@@ -10,7 +10,7 @@ v = TestFunction(V)
 
 f = Function(V)
 x = SpatialCoordinate(mesh)
-f.project(as_vector([x[1], x[0]]))
+assemble(42*dot(v, FacetNormal(mesh))*ds, tensor=f)
 
 r = Function(V)
 a = inner(u, v)*dx
@@ -23,5 +23,7 @@ phi_d = TestFunction(V_d)
 r_d = assemble(inner(r, phi_d)*dx)
 
 ref = assemble(inner(f, phi_d)*dx)
+
+projection_rd = project(f, V_d)
 
 print(errornorm(r_d, ref))
