fa50980bf4a1a11ccef64628e3003184a47cb305
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-06-22 17:26:34 +0100

    Test lowest order convergence with PP

--- benchmarks/helmholtz_bchmk.py
+++ benchmarks/helmholtz_bchmk.py
@@ -1,14 +1,18 @@
 from __future__ import absolute_import, division
 from hybrid_solvers import *
 from firedrake import *
+import numpy as np
 
 
-problem = HybridMixedHelmholtzProblem(degree=1, resolution=8, hexes=False)
-solver = HybridSolver(problem)
-solver.solve()
-sigma, u = solver.computed_solution
-exact = problem.exact_solution
-sigma.rename("velocity")
-u.rename("pressure")
+errs = []
+for i in range(2, 8):
+    problem = HybridMixedHelmholtzProblem(degree=1, resolution=i, hexes=False)
+    solver = HybridSolver(problem, post_process=True)
+    solver.solve()
+    u = solver.computed_solution
+    exact = problem.exact_solution
+    err = errornorm(u, exact)
+    errs.append(err)
 
-File("test.pvd").write(u, sigma, exact)
+errs = np.array(errs)
+print np.log2(errs[:-1] / errs[1:])[-1]
