9170ad025ada343f15f3b3724bbe3e7763fc4c7a
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-06-24 14:09:27 +0100

    Add print statements and commented plotting for reference

--- convergence_tests/3D_helmholtz_extr.py
+++ convergence_tests/3D_helmholtz_extr.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import, division
 
 from firedrake import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def run_hybrid_extr_helmholtz(degree, res, quads):
+def run_hybrid_extr_helmholtz(degree, res, quads=False, write=False):
     nx = 2 ** res
     ny = 2 ** res
-    nz = 2 ** (res - 1)
+    nz = 2 ** res
     h = 0.2 / nz
     base = UnitSquareMesh(nx, ny, quadrilateral=quads)
     mesh = ExtrudedMesh(base, layers=nz, layer_height=h)
@@ -29,12 +31,15 @@ def run_hybrid_extr_helmholtz(degree, res, quads):
     U = FunctionSpace(mesh, "DG", degree)
     W = V * U
 
-    x, y, z = SpatialCoordinate(mesh)
+    x = SpatialCoordinate(mesh)
     f = Function(U)
-    f.interpolate(Expression("(1+38*pi*pi)*sin(x[0]*pi*2)*sin(x[1]*pi*3)*sin(x[2]*pi*5)"))
+    f.interpolate((1+38*pi*pi)*sin(x[0]*pi*2)*sin(x[1]*pi*3)*sin(x[2]*pi*5))
     exact = Function(U)
-    exact.interpolate(Expression("sin(x[0]*pi*2)*sin(x[1]*pi*3)*sin(x[2]*pi*5)"))
+    exact.interpolate(sin(x[0]*pi*2)*sin(x[1]*pi*3)*sin(x[2]*pi*5))
     exact.rename("exact")
+    exact_flux = Function(V)
+    exact_flux.project(-grad(sin(x[0]*pi*2)*sin(x[1]*pi*3)*sin(x[2]*pi*5)))
+    exact_flux.rename("exact_flux")
 
     sigma, u = TrialFunctions(W)
     tau, v = TestFunctions(W)
@@ -42,22 +47,101 @@ def run_hybrid_extr_helmholtz(degree, res, quads):
     a = dot(sigma, tau)*dx + u*v*dx + div(sigma)*v*dx - div(tau)*u*dx
     L = f*v*dx
     w = Function(W)
-    params = {'mat_type': 'matfree',
-              'ksp_type': 'preonly',
+    params = {'ksp_type': 'preonly',
+              'mat_type': 'matfree',
               'pc_type': 'python',
               'pc_python_type': 'firedrake.HybridizationPC',
-              'hybridization': {'ksp_type': 'preonly',
-                                'pc_type': 'lu',
+              'hybridization': {'ksp_type': 'cg',
+                                'pc_type': 'hypre',
+                                'pc_hypre_type': 'boomeramg',
+                                'ksp_rtol': 1e-8,
+                                'ksp_monitor': True,
                                 'hdiv_residual': {'ksp_type': 'cg',
-                                                  'ksp_rtol': 1e-14},
+                                                  'pc_type': 'bjacobi',
+                                                  'sub_pc_type': 'ilu',
+                                                  'ksp_rtol': 1e-8,
+                                                  'ksp_monitor': True},
                                 'use_reconstructor': True}}
     solve(a == L, w, solver_parameters=params)
     sigma_h, u_h = w.split()
     sigma_h.rename("flux")
     u_h.rename("pressure")
 
-    print errornorm(u_h, exact)
+    err_s = errornorm(u_h, exact)
+    err_f = errornorm(sigma_h, exact_flux)
 
-    File("3D-hybrid.pvd").write(sigma_h, u_h, exact)
+    if write:
+        File("3D-hybrid.pvd").write(sigma_h, u_h, exact, exact_flux)
+        return
+    else:
+        return (err_s, err_f), mesh
 
-run_hybrid_extr_helmholtz(0, 5, quads=False)
+ref_levels = range(6, 8)
+degree = 0
+errRT_u = []
+errRT_sigma = []
+errRTCF_u = []
+errRTCF_sigma = []
+mRT = []
+mRTCF = []
+for i in ref_levels:
+    e, mrt = run_hybrid_extr_helmholtz(degree=degree,
+                                       res=i, quads=False)
+    rt_err_s, rt_err_f = e
+    ertc, mrtcf = run_hybrid_extr_helmholtz(degree=degree,
+                                            res=i, quads=True)
+    rtcf_err_s, rtcf_err_f = ertc
+
+    mRT.append(mrt)
+    mRTCF.append(mrtcf)
+    errRT_u.append(rt_err_s)
+    errRT_sigma.append(rt_err_f)
+    errRTCF_u.append(rtcf_err_s)
+    errRTCF_sigma.append(rtcf_err_f)
+
+errRT_u = np.asarray(errRT_u)
+errRT_sigma = np.asarray(errRT_sigma)
+errRTCF_u = np.asarray(errRTCF_u)
+errRTCF_sigma = np.asarray(errRTCF_sigma)
+rtdof = mRT[-1].topology.num_cells()*6
+rtcfdof = mRTCF[-1].topology.num_cells()*7
+
+print "RT Dof count: %d" % rtdof
+print "RT perr: %f" % errRT_u[-1]
+print "RT uerr: %f" % errRT_sigma[-1]
+print "RT p EOC: %f" % np.log2(errRT_u[:-1]/errRT_u[1:])[-1]
+print "RT u EOC: %f" % np.log2(errRT_sigma[:-1]/errRT_sigma[1:])[-1]
+
+print "RTCF Dof count: %d" % rtcfdof
+print "RTCF perr %f" % errRTCF_u[-1]
+print "RTCF uerr %f" % errRTCF_sigma[-1]
+print "RTCF p EOC: %f" % np.log2(errRTCF_u[:-1]/errRTCF_u[1:])[-1]
+print "RTCF u EOC: %f" % np.log2(errRTCF_sigma[:-1]/errRTCF_sigma[1:])[-1]
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+
+# res = [2 ** r for r in ref_levels]
+
+# ax.loglog(res, errRT_u, color='r', marker='o',
+#           linestyle='-', linewidth='2',
+#           label='RT0-DG0-p')
+# ax.loglog(res, errRT_sigma, color='r', marker='^',
+#           linestyle='-', linewidth='2',
+#           label='RT0-DG0-u')
+# ax.loglog(res, errRTCF_u, color='b', marker='o',
+#           linestyle='-', linewidth='2',
+#           label='RTCF0-DQ0-p')
+# ax.loglog(res, errRTCF_sigma, color='b', marker='^',
+#           label='RTCF0-DQ0-u')
+# ax.grid(True)
+# plt.title("Resolution test for lowest order H-RT and H-RTCF methods")
+# plt.xlabel("Mesh resolution in all spatial directions $2^r$")
+# plt.ylabel("$L_2$-error")
+# plt.gca().invert_xaxis()
+# plt.legend(loc=1)
+# font = {'family': 'normal',
+#         'weight': 'bold',
+#         'size': 22}
+# plt.rc('font', **font)
+# plt.show()
