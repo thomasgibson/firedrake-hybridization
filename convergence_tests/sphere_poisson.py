4f1650b05447dfe8702f209f880939fbedfa4473
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-09-15 17:25:13 +0100

    Write to csv file - Mixed Poisson

--- convergence_tests/sphere_poisson.py
+++ convergence_tests/sphere_poisson.py
@@ -1,26 +1,28 @@
 from __future__ import absolute_import, division
 from firedrake import *
-import matplotlib.pyplot as plt
 import numpy as np
+import csv
+
+
+def l2_error(a, b):
+    return sqrt(assemble(inner(a - b, a - b)*dx))
 
 
 def poisson_sphere(MeshClass, refinement, hdiv_space, degree):
     """Test hybridizing lowest order mixed methods on a sphere."""
-    mesh = MeshClass(refinement_level=refinement)
+    mesh = MeshClass(refinement_level=refinement, degree=degree + 3)
     mesh.init_cell_orientations(Expression(("x[0]", "x[1]", "x[2]")))
     x, y, z = SpatialCoordinate(mesh)
 
     V = FunctionSpace(mesh, hdiv_space, degree + 1)
     U = FunctionSpace(mesh, "DG", degree)
     W = U * V
-    Ue = FunctionSpace(mesh, "CG", degree + 3)
-    Ve = FunctionSpace(mesh, hdiv_space, 3*(degree + 1))
 
     f = Function(U)
     f.interpolate(x*y*z)
 
-    u_exact = Function(Ue).interpolate(x*y*z/12.0)
-    sigma_exact = Function(Ve).project(-grad(x*y*z/12.0))
+    u_exact = Function(FunctionSpace(mesh, "DG", degree + 2)).interpolate(x*y*z/12.0)
+    sigma_exact = Function(VectorFunctionSpace(mesh, "CG", degree + 4)).project(-grad(u_exact))
 
     u, sigma = TrialFunctions(W)
     v, tau = TestFunctions(W)
@@ -31,26 +33,27 @@ def poisson_sphere(MeshClass, refinement, hdiv_space, degree):
 
     nullsp = MixedVectorSpaceBasis(W, [VectorSpaceBasis(constant=True), W[1]])
     params = {'mat_type': 'matfree',
-              'ksp_type': 'preonly',
+              'ksp_type': 'gmres',
+              'ksp_monitor_true_residual': True,
               'pc_type': 'python',
               'pc_python_type': 'firedrake.HybridizationPC',
-              'hybridization': {'ksp_type': 'preonly',
+              'hybridization': {'ksp_type': 'gmres',
                                 'pc_type': 'lu',
                                 'pc_factor_mat_solver_package': 'mumps',
+                                'ksp_monitor': True,
                                 'hdiv_residual': {'ksp_type': 'cg',
-                                                  'ksp_monitor': True,
-                                                  'ksp_rtol': 1e-14,
                                                   'pc_type': 'bjacobi',
-                                                  'sub_pc_type': 'ilu'},
+                                                  'sub_pc_type': 'ilu',
+                                                  'ksp_monitor': True},
                                 'use_reconstructor': True}}
     solve(a == L, w, nullspace=nullsp, solver_parameters=params)
     u_h, sigma_h = w.split()
-    error_u = errornorm(u_exact, u_h)
-    error_sigma = errornorm(sigma_exact, sigma_h)
+    error_u = l2_error(u_exact, u_h)
+    error_sigma = l2_error(sigma_exact, sigma_h)
     return error_u, error_sigma
 
 
-degree = 0
+degree = 1
 mesh = {"BDM": UnitIcosahedralSphereMesh,
         "RT": UnitIcosahedralSphereMesh,
         "RTCF": UnitCubedSphereMesh}
@@ -60,11 +63,14 @@ bdmerr_u = []
 bdmerr_sigma = []
 rtcferr_u = []
 rtcferr_sigma = []
-trimesh_cw = []
-cubemesh_cw = []
-for i in range(2, 8):
-    trimesh_cw.append(sqrt((4.0*pi)/mesh["RT"](i).topology.num_cells()))
-    cubemesh_cw.append(sqrt((4.0*pi)/mesh["RTCF"](i).topology.num_cells()))
+volTri = []
+volQuad = []
+
+# Local max is 7
+ref_levels = range(2, 7)
+for i in ref_levels:
+    volTri.append(sqrt((4.0*pi)/mesh["RT"](i).topology.num_cells()))
+    volQuad.append(sqrt((4.0*pi)/mesh["RTCF"](i).topology.num_cells()))
     rt_u_err, rt_s_err = poisson_sphere(mesh["RT"], i, "RT", degree)
     bdm_u_err, bdm_s_err = poisson_sphere(mesh["BDM"], i, "BDM", degree)
     rtcf_u_err, rtcf_s_err = poisson_sphere(mesh["RTCF"], i, "RTCF", degree)
@@ -83,60 +89,36 @@ bdmerr_sigma = np.asarray(bdmerr_sigma)
 rtcferr_u = np.asarray(rtcferr_u)
 rtcferr_sigma = np.asarray(rtcferr_sigma)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-dh = np.asarray(cubemesh_cw)
 k = degree + 1
-dh_arry = dh ** k
-dh_arry = 0.001 * dh_arry
+res = [1/(2 ** r) for r in ref_levels]
+dhk = np.array(res) ** k
+dhk1 = np.array(res) ** (k + 1)
+dh_arry_k = (0.1 ** k) * dhk
+dh_arry_k1 = (0.1 ** k) * dhk1
 
-orange = '#FF6600'
-lw = '5'
-ms = 15
-
-if k == 1:
-    dhlabel = '$\propto \Delta h$'
-else:
-    dhlabel = '$\propto \Delta h^%d$' % k
-
-print "RT EOC for p: %f" % np.log2(rterr_u[:-1]/rterr_u[1:])[-1]
-print "BDM EOC for p: %f" % np.log2(bdmerr_u[:-1]/bdmerr_u[1:])[-1]
-print "RTCF EOC for p: %f" % np.log2(rtcferr_u[:-1]/rtcferr_u[1:])[-1]
-
-print "RT EOC for u: %f" % np.log2(rterr_sigma[:-1]/rterr_sigma[1:])[-1]
-print "BDM EOC for u: %f" % np.log2(bdmerr_sigma[:-1]/bdmerr_sigma[1:])[-1]
-print "RTCF EOC for u: %f" % np.log2(rtcferr_sigma[:-1]/rtcferr_sigma[1:])[-1]
-
-ax.loglog(trimesh_cw, rterr_u, color='r', marker='o',
-          linestyle='-', linewidth=lw, markersize=ms,
-          label='$RT_1$ $DG_0$ $p_h$')
-ax.loglog(trimesh_cw, rterr_sigma, color='b', marker='^',
-          linestyle='-', linewidth=lw, markersize=ms,
-          label='$RT_1$ $DG_0$ $u_h$')
-ax.loglog(trimesh_cw, bdmerr_u, color='c', marker='o',
-          linestyle='--', linewidth=lw, markersize=ms,
-          label='$BDM_1$ $DG_0$ $p_h$')
-ax.loglog(trimesh_cw, bdmerr_sigma, color=orange, marker='^',
-          linestyle='--', linewidth=lw, markersize=ms,
-          label='$BDM_1$ $DG_0$ $u_h$')
-ax.loglog(cubemesh_cw, rtcferr_u, color='g', marker='o',
-          linewidth=lw, markersize=ms,
-          label='$RTCF_1$ $DQ_0$ $p_h$')
-ax.loglog(cubemesh_cw, rtcferr_sigma, color='m', marker='^',
-          linewidth=lw, markersize=ms,
-          label='$RTCF_1$ $DQ_0$ $u_h$')
-ax.loglog(trimesh_cw, dh_arry, color='k', linestyle=':',
-          linewidth=lw, label=dhlabel)
-ax.grid(True)
-plt.title("Resolution test for LO H-RT, H-BDM, and H-RTCF methods")
-plt.xlabel("Cell width $\Delta h$")
-plt.ylabel("$L^2$-error against projected exact solution")
-plt.gca().invert_xaxis()
-plt.legend(loc=2)
-font = {'family': 'normal',
-        'weight': 'bold',
-        'size': 28}
-plt.rc('font', **font)
-plt.gca().invert_xaxis()
-plt.show()
+fieldnames = ['cell_vol_tri',
+              'cell_vol_quad',
+              'RT_u_err',
+              'BDM_u_err',
+              'RTCF_u_err',
+              'RT_sigma_err',
+              'BDM_sigma_err',
+              'RTCF_sigma_err',
+              'dh_k',
+              'dh_k1']
+data = [volTri,
+        volQuad,
+        rterr_u,
+        bdmerr_u,
+        rtcferr_u,
+        rterr_sigma,
+        bdmerr_sigma,
+        rtcferr_sigma,
+        dh_arry_k,
+        dh_arry_k1]
+csv_file = open('2d_poisson_sphere_d%d.csv' % k, 'w')
+csvwriter = csv.writer(csv_file)
+csvwriter.writerow(fieldnames)
+for data in zip(*data):
+    csvwriter.writerow(data)
+csv_file.close()
