adb31f4f97f48c8d4207a11511e81c9c47359d86
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-02-09 19:58:25 +0000

    Skamarock and Klemp test case

---
+++ compressible_tests/sk_nonlinear.py
@@ -0,0 +1,136 @@
+"""
+Credit for demo setup: Gusto development team
+"""
+from gusto import *
+import itertools
+from firedrake import as_vector, SpatialCoordinate, PeriodicIntervalMesh, \
+    ExtrudedMesh, exp, sin, Function
+from hybridization import HybridizedCompressibleSolver
+import numpy as np
+import sys
+
+if '--run-test' in sys.argv:
+    dt = 6.
+    delta = 2000.
+    tmax = dt
+else:
+    # delta/dt combinations:
+    # delta = 2000., dt = 6;
+    # delta = 1000., dt = 5;
+    # delta = 500., dt = 4;
+    # delta = 250., dt = 3;
+    # delta = 125., dt = 2;
+    dt = 5.
+    delta = 1000.
+    tmax = 3600.
+
+if '--hybrid' in sys.argv:
+    hybrid = True
+else:
+    hybrid = False
+
+H = 1.0e4  # Height position of the model top
+L = 3.0e5
+
+nlayers = int(2*H/delta)  # horizontal layers
+columns = int(L/delta)    # number of columns
+m = PeriodicIntervalMesh(columns, L)
+
+# build volume mesh
+mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
+
+points_x = np.linspace(0., L, 100)
+points_z = [H/2.]
+points = np.array([p for p in itertools.product(points_x, points_z)])
+
+fieldlist = ['u', 'rho', 'theta']
+timestepping = TimesteppingParameters(dt=dt)
+output = OutputParameters(dirname='sk_nonlinear', dumpfreq=1, dumplist=['u'],
+                          perturbation_fields=['theta', 'rho'],
+                          point_data=[('theta_perturbation', points)])
+parameters = CompressibleParameters()
+diagnostics = Diagnostics(*fieldlist)
+diagnostic_fields = [CourantNumber()]
+
+state = State(mesh, vertical_degree=1, horizontal_degree=1,
+              family="CG",
+              timestepping=timestepping,
+              output=output,
+              parameters=parameters,
+              diagnostics=diagnostics,
+              fieldlist=fieldlist,
+              diagnostic_fields=diagnostic_fields)
+
+# Initial conditions
+u0 = state.fields("u")
+rho0 = state.fields("rho")
+theta0 = state.fields("theta")
+
+# spaces
+Vu = u0.function_space()
+Vt = theta0.function_space()
+Vr = rho0.function_space()
+
+# Thermodynamic constants required for setting initial conditions
+# and reference profiles
+g = parameters.g
+N = parameters.N
+p_0 = parameters.p_0
+c_p = parameters.cp
+R_d = parameters.R_d
+kappa = parameters.kappa
+
+x, z = SpatialCoordinate(mesh)
+
+# N^2 = (g/theta)dtheta/dz => dtheta/dz = theta N^2g => theta=theta_0exp(N^2gz)
+Tsurf = 300.
+thetab = Tsurf*exp(N**2*z/g)
+
+theta_b = Function(Vt).interpolate(thetab)
+rho_b = Function(Vr)
+
+# Calculate hydrostatic Pi
+compressible_hydrostatic_balance(state, theta_b, rho_b)
+
+a = 5.0e3
+deltaTheta = 1.0e-2
+theta_pert = deltaTheta*sin(np.pi*z/H)/(1 + (x - L/2)**2/a**2)
+theta0.interpolate(theta_b + theta_pert)
+rho0.assign(rho_b)
+u0.project(as_vector([20.0, 0.0]))
+
+state.initialise([('u', u0),
+                  ('rho', rho0),
+                  ('theta', theta0)])
+state.set_reference_profiles([('rho', rho_b),
+                              ('theta', theta_b)])
+
+# Set up advection schemes
+ueqn = EulerPoincare(state, Vu)
+rhoeqn = AdvectionEquation(state, Vr, equation_form="continuity")
+supg = True
+if supg:
+    thetaeqn = SUPGAdvection(state, Vt, supg_params={"dg_direction": "horizontal"},
+                             equation_form="advective")
+else:
+    thetaeqn = EmbeddedDGAdvection(state, Vt, equation_form="advective")
+
+advected_fields = []
+advected_fields.append(("u", ThetaMethod(state, u0, ueqn)))
+advected_fields.append(("rho", SSPRK3(state, rho0, rhoeqn)))
+advected_fields.append(("theta", SSPRK3(state, theta0, thetaeqn)))
+
+# Set up linear solver
+if hybrid:
+    linear_solver = HybridizedCompressibleSolver(state)
+else:
+    linear_solver = CompressibleSolver(state)
+
+# Set up forcing
+compressible_forcing = CompressibleForcing(state)
+
+# build time stepper
+stepper = CrankNicolson(state, advected_fields, linear_solver,
+                        compressible_forcing)
+
+stepper.run(t=0, tmax=tmax)
