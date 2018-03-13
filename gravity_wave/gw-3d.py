2b071473ca98f36b3396e834ef0cf0cdd361aed6
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-03-13 22:02:29 +0000

    Add argument parser for customized runs

--- gravity_wave/gw-3d.py
+++ gravity_wave/gw-3d.py
@@ -1,34 +1,103 @@
 from firedrake import *
+from firedrake.petsc import PETSc
 from function_spaces import construct_spaces
 from solver import GravityWaveSolver
-from gaussian import MultipleGaussianExpression
+from argparse import ArgumentParser
 import math
 import numpy as np
+import sys
 
 
-# Spherical shell (Earth with layers)
+PETSc.Log.begin()
+parser = ArgumentParser(description="""Linear gravity wave system.""",
+                        add_help=False)
+
+parser.add_argument("--refinements",
+                    default=3,
+                    type=int,
+                    choices=[3, 4, 5, 6, 7],
+                    help=("Number of refinements when generating the "
+                          "spherical base mesh."))
+
+parser.add_argument("--extrusion_levels",
+                    default=4,
+                    type=int,
+                    help="Number of vertical levels in the extruded mesh.")
+
+parse.add_argument("--cubedsphere",
+                   action="store_true",
+                   help="Use an extruded cubed-sphere mesh.")
+
+parser.add_argument("--hybridization",
+                    action="store_true",
+                    help=("Use a hybridized mixed method to solve the "
+                          "gravity wave equations."))
+
+parser.add_argument("--order",
+                    default=1,
+                    type=int,
+                    help="Order of the compatible mixed method.")
+
+parser.add_argument("--test",
+                    action="store_true",
+                    help="Enable a quick test run.")
+
+parser.add_argument("--profile",
+                    action="store_true",
+                    help="Turn on profiler for simulation timings.")
+
+parser.add_argument("--long_run",
+                    action="store_true",
+                    help="Run simulation for 100 time-steps")
+
+parser.add_argument("--output",
+                    action="store_true",
+                    help="Write output.")
+
+parser.add_argument("--help",
+                    action="store_true",
+                    help="Show help.")
+
+args, _ = parser.parse_known_args()
+
+if args.help:
+    help = parser.format_help()
+    PETSc.Sys.Print("%s\n" % help)
+    sys.exit(1)
+
+if args.profile:
+    # Ensures accurate timing of parallel loops
+    parameters["pyop2_options"]["lazy_evaluation"] = False
+
 # Number of vertical layers
-nlayer = 40
-# Radius of earth
+nlayer = args.extrusion_levels
+
+# Radius of earth (scaled)
 r_earth = 6.371e6/125.0
+
 # Thickness of spherical shell
 thickness = 1.0e4
 
-order = 1
+order = args.order
+reflvl = args.refinements
 
-base = IcosahedralSphereMesh(r_earth,
-                             refinement_level=5,
-                             degree=3)
+if args.cubedsphere:
+    base = CubedSphereMesh(r_earth,
+                           refinement_level=reflvl,
+                           degree=3)
+else:
+    base = IcosahedralSphereMesh(r_earth,
+                                 refinement_level=reflvl,
+                                 degree=3)
+
+# Extruded mesh
 mesh = ExtrudedMesh(base, extrusion_type='radial',
                     layers=nlayer, layer_height=thickness/nlayer)
-num_cells = mesh.cell_set.size
-solver_type = "AMG"
 
-g = MultipleGaussianExpression(1, r_earth, thickness)
-expression = Expression(str(g))
-
+# Compatible finite element spaces
 W2, W3, Wb, _ = construct_spaces(mesh, order=order)
 
+# Initial condition for velocity
 u0 = Function(W2)
 x = SpatialCoordinate(mesh)
 u_max = 20.
@@ -36,6 +105,7 @@ uexpr = as_vector([-u_max*x[1]/r_earth,
                    u_max*x[0]/r_earth, 0.0])
 u0.project(uexpr)
 
+# Initial condition for buoyancy
 lamda_c = 2.0*np.pi/3.0
 phi_c = 0.0
 W_CG1 = FunctionSpace(mesh, "CG", 1)
@@ -57,27 +127,47 @@ s = (d**2)/(d**2 + r**2)
 bexpr = deltaTheta*s*sin(2*np.pi*z/L_z)
 b0.interpolate(bexpr)
 
+# Initial condition for pressure
 p0 = Function(W3).assign(0.0)
 
+# Physical constants and time-stepping parameters
 c = 300
 N = 0.01
 Omega = 7.292e-5
 nu_cfl = 8.
+num_cells = mesh.cell_set.size
 dx = 2.*r_earth/math.sqrt(3.)*math.sqrt(4.*math.pi/(num_cells))
 dt = nu_cfl/c*dx
 
+# Setup linear solvers
 solver = GravityWaveSolver(W2, W3, Wb, dt, c, N, Omega, r_earth,
-                           monitor=True, hybridization=True,
-                           solver_type=solver_type)
+                           monitor=args.test,
+                           hybridization=args.hybridization)
+
+# Initialize
 solver.initialize(u0, p0, b0)
 
-tmax = dt*100
+if args.test:
+    tmax = 4*dt
+elif args.long_run:
+    tmax = dt*100
+else:
+    tmax = dt*20
+
+# RUN!
 t = 0
-output = File("results/shell/gw3d.pvd")
 u, p, b = solver._state.split()
-output.write(u, p, b)
-while t < tmax:
-    t += dt
-    solver.solve()
-    u, p, b = solver._state.split()
+
+if args.output:
+    output = File("results/gw3d/gw3d.pvd")
     output.write(u, p, b)
+
+    while t < tmax:
+        t += dt
+        solver.solve()
+        u, p, b = solver._state.split()
+        output.write(u, p, b)
+else:
+    while t < tmax:
+        t += dt
+        solver.solve()
