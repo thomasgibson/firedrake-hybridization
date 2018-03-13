7781741a3a9e7ea72328e7507f2b5e968ed604ca
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-03-13 21:10:23 +0000

    Add some comments

--- gravity_wave/solver.py
+++ gravity_wave/solver.py
@@ -14,11 +14,10 @@ class GravityWaveSolver(object):
         self.monitor = monitor
         if solver_type == "AMG":
             self.up_params = self.amg_paramters
-        elif solver_type == "GMG":
-            self.up_params = self.gmg_parameters
         else:
             raise ValueError("Unknown inner solver type")
 
+        # Timestepping parameters and physical constants
         self._dt = dt
         self._c = c
         self._N = N
@@ -27,28 +26,31 @@ class GravityWaveSolver(object):
         self._dt_half_c2 = Constant(0.5*dt*c**2)
         self._omega_N2 = Constant((0.5*dt*N)**2)
 
+        # Compatible finite element spaces
         self._Wmixed = W2 * W3
         self._W2 = self._Wmixed.sub(0)
         self._W3 = self._Wmixed.sub(1)
         self._Wb = Wb
 
+        # Functions for state solutions
         self._up = Function(self._Wmixed)
         self._b = Function(self._Wb)
         self._btmp = Function(self._Wb)
 
         self._state = Function(self._W2 * self._W3 * self._Wb, name="State")
 
+        # Outward normal vector
         mesh = self._W3.mesh()
-        # Not sure why this doesn't work...
-        # self._khat = CellNormal(mesh)
         x = SpatialCoordinate(mesh)
         R = sqrt(inner(x, x))
         self._khat = interpolate(x/R, mesh.coordinates.function_space())
 
+        # Coriolis term
         fexpr = 2*Omega*x[2]/R
         Vcg = FunctionSpace(mesh, "CG", 1)
         self._f = interpolate(fexpr, Vcg)
 
+        # Construct linear solvers
         self._build_up_solver()
         self._build_b_solver()
 
@@ -56,6 +58,7 @@ class GravityWaveSolver(object):
     def amg_paramters(self):
         """
         """
+
         if self.hybridization:
             params = {'ksp_type': 'preonly',
                       'mat_type': 'matfree',
@@ -97,18 +100,28 @@ class GravityWaveSolver(object):
         """
         """
 
+        # Current state
         u0, p0, b0 = self._state.split()
 
+        # Test and trial functions for the velocity-pressure increment
+        # system (with strong no-slip boundary conditions on the top
+        # and bottom of the atmospheric domain)
         utest, ptest = TestFunctions(self._Wmixed)
         utrial, ptrial = TrialFunctions(self._Wmixed)
         bcs = [DirichletBC(self._Wmixed.sub(0), 0.0, "bottom"),
                DirichletBC(self._Wmixed.sub(0), 0.0, "top")]
 
+        def outward(u):
+            return cross(self._khat, u)
+
+        # Linear gravity wave system for the velocity and pressure
+        # increments (buoyancy has been eliminated in the discrete
+        # equations since there is no orography)
         a_up = (ptest*ptrial
                 + self._dt_half_c2*ptest*div(utrial)
                 - self._dt_half*div(utest)*ptrial
                 + (dot(utest, utrial)
-                   + self._dt_half*dot(utest, self._f*cross(self._khat, utrial))
+                   + self._dt_half*dot(utest, self._f*outward(utrial))
                    + self._omega_N2
                     * dot(utest, self._khat)
                     * dot(utrial, self._khat))) * dx
@@ -117,6 +130,7 @@ class GravityWaveSolver(object):
                 + self._dt_half*dot(utest, self._khat*b0)
                 + ptest*p0) * dx
 
+        # Set up linear solver
         up_problem = LinearVariationalProblem(a_up, L_up, self._up, bcs=bcs)
         up_solver = LinearVariationalSolver(up_problem,
                                             solver_parameters=self.up_params)
@@ -126,8 +140,10 @@ class GravityWaveSolver(object):
         """
         """
 
+        # Computed velocity perturbation
         u0, _, _ = self._state.split()
 
+        # Expression for buoyancy reconstruction
         btest = TestFunction(self._Wb)
         L_b = dot(btest*self._khat, u0) * dx
         a_b = btest*TrialFunction(self._Wb) * dx
@@ -139,6 +155,7 @@ class GravityWaveSolver(object):
         if self.monitor:
             b_params['ksp_monitor_true_residual'] = True
 
+        # Solver for buoyancy update
         b_solver = LinearVariationalSolver(b_problem,
                                            solver_parameters=b_params)
         self.b_solver = b_solver
@@ -146,6 +163,7 @@ class GravityWaveSolver(object):
     def initialize(self, u, p, b):
         """
         """
+
         u0, p0, b0 = self._state.split()
         u0.assign(u)
         p0.assign(p)
@@ -154,9 +172,11 @@ class GravityWaveSolver(object):
     def solve(self):
         """
         """
+
+        # Previous states
         un, pn, bn = self._state.split()
 
-        # Solve for u and p
+        # Solve for u and p updates
         self._up.assign(0.0)
         self._b.assign(0.0)
         self.up_solver.solve()
