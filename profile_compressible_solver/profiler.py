87d145163700a2295e42736e8d27567c7f9df880
Thomas Gibson <t.gibson15@imperial.ac.uk>
2019-02-14 18:14:45 +0000

    Just apply forcing

--- profile_compressible_solver/profiler.py
+++ profile_compressible_solver/profiler.py
@@ -57,23 +57,7 @@ class Profiler(GCN):
 
         with timed_stage("Apply forcing terms"):
             self.forcing.apply((1-alpha)*dt, state.xn, state.xn,
-                               state.xstar, implicit=False)
-
-        with timed_stage("Advection"):
-            for name, advection in self.active_advection:
-                # first computes ubar from state.xn and state.xnp1
-                advection.update_ubar(state.xn, state.xnp1, alpha)
-                # advects a field from xstar and puts result in xp
-                advection.apply(self.xstar_fields[name], self.xp_fields[name])
-
-        state.xrhs.assign(0.)
-
-        with timed_stage("Apply forcing terms"):
-            self.forcing.apply(alpha*dt, state.xp, state.xnp1,
-                               state.xrhs, implicit=True,
-                               incompressible=False)
-
-        state.xrhs -= state.xnp1
+                               state.xrhs, implicit=False)
 
         logger.info("||b|| = %s" % state.xrhs.dat.norm)
 
